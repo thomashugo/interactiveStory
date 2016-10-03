4e8c53c1b71b8f99ba5851f4e9c10119d04d70ca
Thomas Hugo <thomas@thomas-rmpb-2015.local>
2016-10-03 16:08:21 -0700

    Model refactored, text field animated.

--- interactiveStory/ViewController.swift
+++ interactiveStory/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    enum NameError: Error {
+        case NoName
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,11 +32,55 @@ class ViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "startAdventure" {
-            if let pageController = segue.destination as? PageController {
-                pageController.page = Adventure.story
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw NameError.NoName
+                    }
+                    if let pageController = segue.destination as? PageController {
+                        pageController.page = Adventure.story(name: name)
+                    }
+                    
+                }
+            } catch NameError.NoName {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start your story", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error)")
             }
         }
     }
 
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfoDict = notification.userInfo, let keyboardFrameValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardFrameValue.cgRectValue
+            
+            UIView.animate(withDuration: 0.8) {
+                self.textFieldBottomConstraint.constant = keyboardFrame.size.height + 10
+                self.view.layoutIfNeeded()
+            }
+            
+        }
+    }
+    
+    func keyboardWillHide() {
+            UIView.animate(withDuration: 0.8) {
+                self.textFieldBottomConstraint.constant = 40
+                self.view.layoutIfNeeded()
+            }
+            
+        }
+    
+    
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
